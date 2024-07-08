296e1b44c3aca4076bd08406dd3c99aa0a4db65d
Daniel <panturasd@gmail.com>
2024-07-09 01:03:02 +0800

    ...

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -3,6 +3,7 @@ import {
   createUser,
   loginUser,
   getUser,
+  getHello,
 } from "../controllers/userController.js";
 import authMiddleware from "../utils/auth.js";
 const userRouter = express.Router();
@@ -10,5 +11,6 @@ const userRouter = express.Router();
 userRouter.post("/register", createUser);
 userRouter.post("/login", loginUser);
 userRouter.get("/user/:id", authMiddleware, getUser);
+userRouter.get("/hello", authMiddleware, getHello);
 
 export default userRouter;
